3bac41270c630e62f32fb2e7abdcc2b111d02e80
sumit-brilworks <sumit.m@brilworks.com>
2024-08-21 11:18:16 +0530

    Added some code by mater
    resolved conflict
    
    Added code form sumit branch
    
    Added navbar

--- demo.js
+++ demo.js
@@ -5,3 +5,8 @@ console.log("Noob developer code");
 // This is anew futher code
 
 // This is also an another code
+
+// some code changes
+// code added by sumit
+// hello some code added
+// This is a code from sumit
